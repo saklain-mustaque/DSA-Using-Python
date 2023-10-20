61cebc6eedd2b729982e28894ebe612a51635eff
Saklain Mustaque <smustaque89@gmail.com>
2023-10-20 10:56:03 +0530

    added assignment 2

---
+++ Assignments/02_Classes_and_objects.py
@@ -0,0 +1,45 @@
+import math
+
+class Person:
+    def __init__(self, name=None, age=None):
+        self.name = name
+        self.age = age
+    def show(self):
+        return f'Name: {self.name}\nAge: {self.age}'
+person = Person('Saklain','25')
+print(person.show())
+
+
+class Circle:
+    def __init__(self, radius = None):
+        self.radius = radius
+
+    def get_radius(self):
+        return self.radius
+    
+    def get_area(self):
+        return math.pi * (self.radius**2)
+    
+    def get_circumference(self):
+        return 2*math.pi*self.radius
+circle = Circle(4)
+print(circle.get_area())
+print(circle.get_circumference())
+
+
+class Team:
+    def __init__(self):
+        self.team_members = []
+            
+    def add_members(self):
+        self.team_members.append(input("Enter the new member name:"))
+    
+    def display_members(self):
+        n = int(input("Enter how many memebers you want to add:"))
+        for n in range(n):
+            self.add_members()
+        if len(self.team_members) != 0:
+            return self.team_members
+    
+team = Team()
+print(team.display_members())
